b7eeac4d86eee05c9c68a737393ea63e4561b26b
D3monizer <vysmol@gmail.com>
2023-04-17 17:54:01 +0300

    feat: edit Edit page

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,15 +1,44 @@
+<script setup>
+import Tasks from '../components/Tasks.vue'
+import { ref, watch } from "vue"
+import { useStore } from 'vuex';
+import { useRoute }  from 'vue-router'
+
+const route = useRoute()
+const store = useStore()
+
+const input = ref(store.state.todos.get(route.query.id)?.content)
+
+watch(() => route.query, () => input.value = store.state.todos.get(route.query.id)?.content)
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <main>
+        <aside>
+            <Tasks/>
+        </aside>
+        <section>
+            <form
+              @submit.prevent="store.commit('updateTodo', {id: $route.query.id, content: input})">
+                <textarea v-model="input" name="todo-editor" id="todo-editor" cols="30" rows="10" required></textarea>
+                <input type="submit" value="Save changes">
+            </form>
+        </section>
+        {{ route.query.id }}
+    </main>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<style scoped>
+    main {
+        width: 95%;
+
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 10px;
+    }
+
+    textarea {
+        width: 100%;
+        height: 100%;
+    }
 </style>
