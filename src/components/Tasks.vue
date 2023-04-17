772d79634f3dbf4c1c17d65584edee7ae5ca00f5
D3monizer <vysmol@gmail.com>
2023-04-17 17:46:33 +0300

    feat: add Tasks component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Task from './Task.vue'
+import {useStore} from 'vuex'
+
+const store = useStore()
+</script>
+
+<template>
+        <ul>
+            <Task v-for="[key, value] in store.state.todos"
+            :key="key"
+            :content="value.content"
+            :id="key"
+            :is-active="value.is_active"/>
+        </ul>
+</template>
+
+<style scoped>
+    ul {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        flex-flow: column;
+        gap: 5px;;
+    }
+</style>
+
