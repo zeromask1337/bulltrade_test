974421280ff07e51a42933696703c5de79e7007a
D3monizer <vysmol@gmail.com>
2023-04-17 17:44:06 +0300

    feat: add Task component

---
+++ src/components/Task.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex'
+
+const store = useStore()
+const props = defineProps({
+    id: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    isActive: {
+        type: Boolean,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <li class="active-todo">
+        <p v-if="isActive">
+            {{ props.content.length > 50 ? props.content.slice(0, 50) + "..." : props.content }}</p>
+        <p v-else><s>{{ props.content }}</s></p>
+
+        <button
+            class="todo-edit-btn"
+            v-if="isActive"
+            @click.stop="store.commit('markDone', props.id)">\/</button>
+        <button
+            class="todo-delete-btn"
+            @click.stop="store.commit('deleteTodo', props.id)">X</button>
+        </li>
+</template>
+
+<style scoped>
+    li.active-todo {
+        padding: 8px;
+        min-height: 50px;
+        width: 500px;
+        border: 1px solid #000;
+        border-radius: 5px;
+
+        display: grid;
+        gap: 10px;
+        grid-template-columns: 350px 1fr 1fr;
+        justify-content: space-between;
+        align-items: center;
+
+        cursor: pointer;
+        /* overflow: hidden; */
+        overflow-wrap: break-word;
+        word-break: break-all;
+    }
+
+    li.active-todo:hover {
+        color: darkorange;
+    }
+
+    .todo-delete-btn, .todo-edit-btn {
+        width: 40px;
+        height: 40px;
+    }
+</style>
