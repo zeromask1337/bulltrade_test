30bb1434fb7290dd70d18cd08db602b5c13281d5
D3monizer <vysmol@gmail.com>
2023-04-17 17:52:44 +0300

    feat: edit homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,60 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { useStore } from 'vuex'
+import Tasks from '../components/Tasks.vue'
+import { ref } from 'vue';
+
+const input = ref("")
+const store = useStore()
+
+function handleSubmit() {
+  store.commit('addTodo', input.value)
+  input.value = ""
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <form @submit.prevent="handleSubmit">
+      <textarea
+        type="text"
+        name="todo_input"
+        id="todo-input"
+        v-model.trim="input"
+        required/>
+      <input type="submit" value="Add todo">
+    </form>
+    <Tasks/>
   </main>
 </template>
+
+<style scoped>
+  main {
+    width: 80%;
+
+    display: flex;
+    flex-flow: column;
+    gap: 30px;
+  }
+
+  form {
+    min-width: 500px;
+
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    gap: 10px;
+  }
+
+  form textarea {
+    width: 100%;
+  }
+
+  form input[type=submit] {
+    width: 70%;
+    height: 30px;
+  }
+
+  textarea {
+    height: 100px;
+  }
+</style>
